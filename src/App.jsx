6b92602ebe3c17c92f1532cbf26de1cd91b7e840
Pompey06 <dmytrouryvskyi@gmail.com>
2025-02-28 03:38:49 +0100

    interface redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import './App.css';
-import Sidebar from './components/Sidebar/Sidebar';
-import ChatWindow from './components/ChatWindow/ChatWindow';
-import { useState } from 'react';
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import ChatWindow from "./components/ChatWindow/ChatWindow";
+import { useState } from "react";
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  // Функция для переключения состояния боковой панели
-  const toggleSidebar = () => {
-    setIsSidebarOpen((prev) => !prev);
-  };
+   // Функция для переключения состояния боковой панели
+   const toggleSidebar = () => {
+      setIsSidebarOpen((prev) => !prev);
+   };
 
-  return (
-    <>
-      <div className="flex wrapper relative pb-11 items-stretch px-6 gap-10 xl:gap-20">
-        {/* Передаём состояние и функцию в Sidebar */}
-        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <ChatWindow isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      </div>
-    </>
-  );
+   return (
+      <>
+         <div className="flex wrapper relative items-stretch">
+            {/* Передаём состояние и функцию в Sidebar */}
+            <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+            <ChatWindow isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+         </div>
+      </>
+   );
 }
 
 export default App;
--- src/components/ChatWindow/MessageInput/MessageInput.jsx
+++ src/components/ChatWindow/MessageInput/MessageInput.jsx
@@ -1,43 +1,46 @@
-import React, { useContext, useState } from 'react';
-import './MessageInput.css';
-import sendIcon from '../../../assets/send.svg';
-import { useTranslation } from 'react-i18next'; 
-import { ChatContext } from '../../../context/ChatContext';
+import React, { useContext, useState } from "react";
+import "./MessageInput.css";
+import sendIcon from "../../../assets/send.svg";
+import newSendIcon from "../../../assets/newSendIcon.svg";
+import { useTranslation } from "react-i18next";
+import { ChatContext } from "../../../context/ChatContext";
 
 export default function MessageInput() {
-  const { t } = useTranslation();
-  const [message, setMessage] = useState('');
+   const { t } = useTranslation();
+   const [message, setMessage] = useState("");
 
-  const { createMessage } = useContext(ChatContext);
+   const { createMessage } = useContext(ChatContext);
 
-  const handleSend = async () => {
-    // Если нет сообщения, можно дополнительно сделать проверку, не отправлять пустую строку
-    if (!message.trim()) return;
-    createMessage(message);
-    console.log(t('messageInput.sentMessage'), message);
-    setMessage(''); 
-  };
+   const handleSend = async () => {
+      // Если нет сообщения, можно дополнительно сделать проверку, не отправлять пустую строку
+      if (!message.trim()) return;
+      createMessage(message);
+      console.log(t("messageInput.sentMessage"), message);
+      setMessage("");
+   };
 
-  const handleKeyDown = (e) => {
-    // Проверяем, что нажата клавиша Enter
-    if (e.key === 'Enter') {
-      handleSend();
-    }
-  };
+   const handleKeyDown = (e) => {
+      // Проверяем, что нажата клавиша Enter
+      if (e.key === "Enter") {
+         handleSend();
+      }
+   };
 
-  return (
-    <div className="message-input mt-auto font-light bg-white flex items-center gap-2">
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        onKeyDown={handleKeyDown}  // Добавили обработчик onKeyDown
-        placeholder={t('messageInput.placeholder')}
-        className="flex-1 p-2 border rounded-lg"
-      />
-      <button onClick={handleSend} className="">
-        <img src={sendIcon} alt={t('messageInput.sendIconAlt')} />
-      </button>
-    </div>
-  );
+   return (
+      <div className="message-input-container">
+         <div className="message-input mt-auto font-light bg-white flex items-center gap-2">
+            <input
+               type="text"
+               value={message}
+               onChange={(e) => setMessage(e.target.value)}
+               onKeyDown={handleKeyDown} // Добавили обработчик onKeyDown
+               placeholder={t("messageInput.placeholder")}
+               className="flex-1 p-2 border rounded-lg"
+            />
+         </div>
+         <button onClick={handleSend} className="">
+            <img src={newSendIcon} alt={t("messageInput.sendIconAlt")} />
+         </button>
+      </div>
+   );
 }
